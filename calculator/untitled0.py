66424f2e9cb85defc13b5b175c55ebca83476f78
rawanayman97 <161222059+rawanayman97@users.noreply.github.com>
2024-02-25 17:09:31 +0200

    Add files via upload

---
+++ calculator/untitled0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 16:53:55 2024
+
+@author: Rawan Ayman
+"""
+
+a = int(input('enter the first number'))
+b = int(input('enter the second number'))
+def calculator(a,b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+calculator(a,b)
